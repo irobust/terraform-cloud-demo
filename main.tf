83d6e75a83ef56545fd76b859121a844c3905de7
Phanupong Permpimol <phanupong@irobust.co.th>
2023-09-03 23:55:12 +0700

    add main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-provider aws {
-        region = "ap-southeast-1"
-    }
+provider "aws" {
+  region = "ap-southeast-1"
+}
 
-    resource "aws_instance" "web" {
-        ami           = "ami-0464f90f5928bccb8"
-        instance_type = "t2.micro"
+resource "aws_instance" "web" {
+  ami           = "ami-0464f90f5928bccb8"
+  instance_type = "t2.micro"
 
-        tags = {
-            Name = "HelloWorld"
-        }
-    }
+  tags = {
+    Name = "HelloWorld"
+  }
+}
